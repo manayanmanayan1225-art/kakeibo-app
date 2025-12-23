a6e911e0765ee8b3ff8a029196d6dea5dc685a5f
Manaya <your-email@example.com>
2025-12-23 20:10:58 +0900

    mac編集文_20251223

--- frontend/src/pages/auth/start/index.tsx
+++ frontend/src/pages/auth/start/index.tsx
@@ -1,22 +1,21 @@
-import React from 'react'
-import './style.scss';
-import { useNavigate } from 'react-router-dom'
+import React from "react";
+import "./style.scss";
+import { useNavigate } from "react-router-dom";
 
 function AuthStart() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
   return (
-
     <div className="start">
       <h1 className="start__title">CoLedger</h1>
-      <div className='coredger-icon-box'>
-        <img src="" alt="coredger-icon" />
+      <div className="start_icon-box">
+        <img src="" alt="start-icon" />
       </div>
       <div className="start__form">
-        <button onClick={() => navigate('/Login')}>sign in</button>
-        <button onClick={() => navigate('/signup')}>sign up</button>
+        <button onClick={() => navigate("/Login")}>sign in</button>
+        <button onClick={() => navigate("/signup")}>sign up</button>
       </div>
     </div>
-  )
+  );
 }
 
-export default AuthStart
+export default AuthStart;
